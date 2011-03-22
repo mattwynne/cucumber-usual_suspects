5f6c8d91022afeb727e6f24f3670d4f365bed0e8
Matt Wynne <matt@mattwynne.net>
2011-03-22 09:58:26 +0000

    Use inheritance to extend RbLanguage instead of monkey-patching

--- lib/cucumber/usual_suspects.rb
+++ lib/cucumber/usual_suspects.rb
@@ -4,14 +4,20 @@ require 'cucumber/usual_suspects/api'
 module Cucumber
   module UsualSuspects
     extend Api
+    
+    module ExpandsStepDefinitionRegexps
+      def register_rb_step_definition(regexp, proc)
+        expanded_regexp = Cucumber::UsualSuspects.expand(regexp)
+        super expanded_regexp, proc
+      end
+    end
   end
   
   module RbSupport
     module RbDsl
       class << self
-        def register_rb_step_definition(regexp, proc)
-          expanded_regexp = Cucumber::UsualSuspects.expand(regexp)
-          @rb_language.register_rb_step_definition(expanded_regexp, proc)
+        def extend_rb_language_with(extension_module)
+          @rb_language.extend(extension_module)
         end
       end
       
@@ -23,3 +29,4 @@ module Cucumber
 end
 
 Cucumber::UsualSuspects.transforms_registry = Cucumber::RbSupport::RbDsl
+Cucumber::RbSupport::RbDsl.extend_rb_language_with(Cucumber::UsualSuspects::ExpandsStepDefinitionRegexps)
